81d7f2d5e67915db91ebc91ede10892321b2b4c4
Alvaro Santos Andres <alvaro.santosandres@gruposantander.com>
2022-01-17 19:45:19 +0100

    Add autocomplete

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -6,4 +6,23 @@ if [[ -z "$(getent group $(id -g))" ]]; then
     sudo groupmod -g $(id -g) cloudnative-docker-cli
 fi
 
+
+# Enable autocomplete commands
+grep 'kubectl completion' "$HOME/.bashrc" > /dev/null 2>&1
+if [[ $? -ne 0 ]] ; then        
+    echo "complete -C /usr/local/bin/aws_completer aws" >> ~/.bashrc   
+    
+    echo "source /usr/local/bin/az.completion.sh" >> ~/.bashrc    
+    
+    echo "source /usr/local/lib/google-cloud-sdk/completion.bash.inc" >> ~/.bashrc
+    
+    echo "source <(kubectl completion bash)" >> ~/.bashrc
+    echo "alias k=kubectl" >> ~/.bashrc
+    echo " complete -F __start_kubectl k" >> ~/.bashrc
+
+    terraform -install-autocomplete
+    
+    vault -autocomplete-install
+fi
+
 exec "$@"
